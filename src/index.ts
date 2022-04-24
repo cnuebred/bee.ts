6257f654d2149dce252cc0aacd9c0f5c55d3f3fd
cnuebred <jonatann01ew@gmai.com>
2022-04-24 10:46:05 +0200

    breakpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { Bee as bee, Component as component } from './bee'
+import { base_style } from './scripts'
 
 export const Bee = bee
 export const Component = component
+
+export const scheme = {
+    base_style: base_style,
+}
